7a3a9cc36a0e6695d3c9e9141d49c18da03089e0
qinqiang2000 <qinqiang2000@qq.com>
2024-04-09 18:02:49 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import requests
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from config.settings import *
+
+app = FastAPI()
+
+
+def chat_doc(msg):
+    data = {"content": "",
+            "notifyParams": [{"type": "openIds", "values": [msg.operatorOpenid]}]}
+    requests.post(YUNZHIJIA_NOTIFY_URL, json=data)
+
+
+class ApiFoxEvent(BaseModel):
+    event: str
+    title: str
+    content: str
+
+
+@app.post("/apifox/{project}")
+async def handle_apifox_event(project: str, request: Request, event: ApiFoxEvent):
+    headers = request.headers
+    logging.info(f"Received headers: {headers}")
+
+    logging.info(f"[{project}]: {event}")
+    # logging.info(f"Title: {event.title}")
+    # logging.info(f"Content: {event.content}")
+
+    return {"message": "Event processed"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=9911)
---
+++ config/settings.py
@@ -0,0 +1,41 @@
+import logging
+import os
+from dotenv import load_dotenv
+
+logging.basicConfig(format='[%(asctime)s %(filename)s:%(lineno)d] %(levelname)s: %(message)s', level=logging.DEBUG)
+
+load_dotenv(override=True)
+
+# openai
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+# azure openai
+AZURE_API_KEY = os.getenv('AZURE_API_KEY')
+AZURE_BASE_URL = os.getenv('AZURE_BASE_URL')
+AZURE_DEPLOYMENT_NAME = os.getenv('AZURE_DEPLOYMENT_NAME')
+AZURE_EBD_DEPLOYMENT_NAME = os.getenv('AZURE_EBD_DEPLOYMENT_NAME')
+
+API_TYPE = os.getenv('API_TYPE')
+
+# 知识库、ai助手、云之家群关系的配置地址
+CONFIG_PATH = "config/config.yml"
+
+# 云之家通知地址
+YUNZHIJIA_NOTIFY_URL = "https://www.yunzhijia.com/gateway/robot/webhook/send?yzjtype=0&yzjtoken=d29fe265a9594811881e86b7e3d8a1e7"
+# 云之家每张卡片消息最大图片数
+MAX_IMG_NUM_IN_CARD_NOTICE = 5
+# 云之家卡片消息模板id
+CARD_NOTICE_TEMPLATE_ID = "64d08cb4e4b07ba2b112b395"
+
+
+YUQUE_AUTH_TOKEN = "aAzViMlNLUtykug7vU5EnmQKvYn9DZGQICFmL3mB"
+YUQUE_NAMESPACE = "nbklz3"  # 语雀团队/空间路径
+YUQUE_BASE_URL = "https://jdpiaozone.yuque.com/api/v2"
+YUQUE_REQUEST_AGENT = "piaozone"
+# assistant file最多文件数-2(要留给faq.md和table.html)
+ASSISTANT_FILE_NUM_LIMIT = 18
+
+
+PIAOZONE_TOKEN_URL = "https://api-dev.piaozone.com/test/base/exception/login/token"
+PIAOZONE_TOKEN_BODY = "U5/yFNQySPUsjrHqSDUFl58fJ7OxHT8W4KWJqK4tLd/ze1/IIFtmActgeM8VxT4uAUn4cW75sKLbaLXPOMFYTVQ+XJDmwosnJ+qsangGMujLo2S3zQqQ/AU8TUd7qgrdYdEKKBLoTIXeCoBA3jjH4u9h+PvFcwfQuSgJbmKwomc="
+PIAOZONE_ADD_SOBOT_DOC_URL = "https://api-dev.piaozone.com/test/portal/m19/customer-service/sobot-doc/with-yuque-slug?access_token="
